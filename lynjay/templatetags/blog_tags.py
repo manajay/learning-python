82cab1df860aaedbbf78b45b7d0b79c9b14d35d5
manajay <manajay.dlj@gmail.com>
2017-10-26 19:58:48 +0800

    add 分类数量

--- lynjay/templatetags/blog_tags.py
+++ lynjay/templatetags/blog_tags.py
@@ -1,16 +1,22 @@
 from django import template
 from ..models import Post
 from ..models import Category
+from django.db.models.aggregates import Count
 register = template.Library()
+
 
 @register.simple_tag
 def get_recent_posts(num=5):
     return Post.objects.all().order_by("-created_time")[:num]
 
+
 @register.simple_tag
 def archives():
-    return Post.objects.dates("created_time","month",order="DESC")
+    return Post.objects.dates("created_time", "month", order="DESC")
+
 
 @register.simple_tag
 def get_categories():
-    return Category.objects.all()
+    # 查询表达式（双下划线）
+    return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+
